6846270770bbb695a7530a35f3e14052e62c8061
shengyeong <chia.shengyeong@gmail.com>
2014-09-27 11:08:25 +0800

    Pusing completed 1:M & M:M

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   #normally, we do not need to be explicit, but because we are using "creator" instead of user, we have to be explicit to state:
   #the foreign_key and also the class name.
   has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories  
 end
