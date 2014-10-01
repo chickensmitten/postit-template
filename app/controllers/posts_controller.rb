3f9ba802167f7547b2ac39f420dc25cb2551239b
shengyeong <chia.shengyeong@gmail.com>
2014-10-01 22:57:25 +0800

    Commit at month 2 week 3 period

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,53 @@
-class PostsController < ApplicationController
+ class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+
   def index
-    @posts = Post.all
+    @posts = Post.all #start an instance variable.
   end
 
   def show
-    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-  
+    @post = Post.new(post_params) #creating private methods to handle strong params
+    @post.creator = User.first #TODO: Change once we have authentication. for now, new posts will be set to the first user.
+
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
-  def update
+  def update    
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids: [])
+    #strong params through require that only permits the following actions above.
+    #use binding pry to check the submission details.
+  end
+
+  def set_post
+    @post = Post.find(params[:id])    
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post #no need to specify the foreign key here as the asumptions are correct.
+
+  validates :body, presence: true
+
 end
